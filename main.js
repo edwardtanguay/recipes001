ec064eedeedaf193031958e2615a33c50a7f2d44
Edward Tanguay (windows) <edward@tanguay.info>
2023-04-19 16:00:07 +0200

    showing recipes with images

--- main.js
+++ main.js
@@ -1,11 +1,41 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
+import recipeData from './data/recipes.json';
+
+const getRowOfRecipesHtml = (recipes) => {
+	let html = '';
+	for (const recipe of recipes) {
+		const elem = `
+<div class="card" style="width: 18rem;">
+  <img src="${recipe.image}" class="img-fluid" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${recipe.title}</h5>
+	<h6>${recipe.author}</h6>
+	<hr/>
+    <p class="card-text">Ingredients: ${recipe.ingredients.join(', ')}</p>
+  </div>
+</div>
+		`;
+		html += elem;
+	}
+	return html;
+};
+
+const getContentHtml = () => {
+  let html = '';
+	const sections = Object.entries(recipeData);
+	for (const section of sections) {
+		const sectionIdCode = section[0];
+		const recipes = section[1];
+
+		const sectionElem = document.querySelector(`#${sectionIdCode}`);
+
+    html +=  getRowOfRecipesHtml(recipes);
+	}
+  return html;
+};
 
 document.querySelector('#app').innerHTML = `
   <div>
-  
+    ${getContentHtml()}
   </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+`;
